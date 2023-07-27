226fd16377f0a3f82b27af50dc9fda2700326fbf
Amon Daniel <amon.daniel.91@gmail.com>
2023-07-27 23:05:59 +0100

    page not loading

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,48 +5,47 @@ import User from "@models/user";
 
 import { connectToDB } from "@utils/database";
 
-
-
 const handler = NextAuth({
   providers: [
     Googleprovider({
-      clientId:process.env.GOOGLE_ID,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    })
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
-
-  async seesion ({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    })
-
-    session.user.id = sessionUser._id.toString();
-  },
-
-  async signIn({ profile}) {
-    try {
-      await connectToDB();
-
-      // check if user already exits
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async seesion({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
 
-      //if not, create new user
-    if(!userExists){
-      await User.create({
-        email: profile.email,
-        username: profile.name.replace(" ", "").toLowerCase(),
-        image: profile.picture
-      })
-    }
+      session.user.id = sessionUser._id.toString();
+    },
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if user already exits
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        //if not, create new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
-})
+});
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
